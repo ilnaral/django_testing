c5d63b8d96091f6676b995b3b82713eb3ba2322f
Ilnara Gadelislamova <loktevailnara1@gmail.com>
2025-03-27 22:30:04 +0500

    vice_versa_6

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -3,18 +3,18 @@ from http import HTTPStatus
 import pytest
 from pytest_django.asserts import assertRedirects
 
-LOGIN_URL = pytest.lazy_fixture('login_url') # type: ignore
-LOGOUT_URL = pytest.lazy_fixture('logout_url') # type: ignore
-SIGNUP_URL = pytest.lazy_fixture('signup_url') # type: ignore
-DETAIL_URL = pytest.lazy_fixture('news_detail_url') # type: ignore
-EDIT_URL = pytest.lazy_fixture('comment_edit_url') # type: ignore
-DELETE_URL = pytest.lazy_fixture('comment_delete_url') # type: ignore
-HOME_URL = pytest.lazy_fixture('news_home_url') # type: ignore
+LOGIN_URL = pytest.lazy_fixture('login_url')  # type: ignore
+LOGOUT_URL = pytest.lazy_fixture('logout_url')  # type: ignore
+SIGNUP_URL = pytest.lazy_fixture('signup_url')  # type: ignore
+DETAIL_URL = pytest.lazy_fixture('news_detail_url')  # type: ignore
+EDIT_URL = pytest.lazy_fixture('comment_edit_url')  # type: ignore
+DELETE_URL = pytest.lazy_fixture('comment_delete_url')  # type: ignore
+HOME_URL = pytest.lazy_fixture('news_home_url')  # type: ignore
 
 
-NOT_AUTHOR_CLIENT = pytest.lazy_fixture('not_author_client') # type: ignore
-ANON_CLIENT = pytest.lazy_fixture('client') # type: ignore
-AUTHOR_CLIENT = pytest.lazy_fixture('author_client') # type: ignore
+NOT_AUTHOR_CLIENT = pytest.lazy_fixture('not_author_client')  # type: ignore
+ANON_CLIENT = pytest.lazy_fixture('client')  # type: ignore
+AUTHOR_CLIENT = pytest.lazy_fixture('author_client')  # type: ignore
 
 EDIT_DELETE_URLS = (DELETE_URL, EDIT_URL)
 ANON_URLS = (DETAIL_URL, HOME_URL, LOGIN_URL, LOGOUT_URL, SIGNUP_URL)
